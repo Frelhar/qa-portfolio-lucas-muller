04a6480e4ab2542cb75b02c6b8c395fcb285779f
Frelhar <132609688+Frelhar@users.noreply.github.com>
2025-08-21 01:35:24 -0700

    Adicionar cypress.config.js

---
+++ ui-automation/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://www.saucedemo.com",
+    video: false,
+    retries: 1,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here if needed
+    },
+  },
+  viewportWidth: 1280,
+  viewportHeight: 800,
+});
